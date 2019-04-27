6f61f367b01c8d85ca11a2c39cb69d8a60adfc34
Michele Bologna <michele.bologna@gmail.com>
2019-04-27 13:40:38 +0200

    Use multi-stage build

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+docker build -t mbologna/docker-znc .
+docker push mbologna/docker-znc
 HOSTNAME=`hostname --fqdn`
 sed -i -e "s/host.name.tld/$HOSTNAME/g" ssmtp.conf
-docker build -t mbologna/docker-znc .
-
+git update-index --assume-unchanged ssmtp.conf
+docker build -t mbologna/docker-znc -f Dockerfile.mbologna .
