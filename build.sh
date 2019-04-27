4e872804471996979c9c41e86a2183a01892ea4b
Michele Bologna <michele.bologna@gmail.com>
2019-04-27 14:28:24 +0200

    Fix conditional building && cleanup

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-
-docker build -t mbologna/docker-znc .
-docker push mbologna/docker-znc
 HOSTNAME=`hostname --fqdn`
-sed -i -e "s/host.name.tld/$HOSTNAME/g" ssmtp.conf
-git update-index --assume-unchanged ssmtp.conf
-docker build -t mbologna/docker-znc -f Dockerfile.mbologna .
+docker build -t mbologna/docker-znc . && docker push mbologna/docker-znc && sed -i -e "s/host.name.tld/$HOSTNAME/g" ssmtp.conf && docker build -t mbologna/docker-znc -f Dockerfile.mbologna . && git checkout ssmtp.conf
